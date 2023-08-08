f6358fb09425ac803e39818c214d061328e6c00b
Sergey Che <rho9cas@gmail.com>
2023-08-08 14:08:06 +0900

    Read-only mode dialog UI

--- src/frontend/lib/general/widgets/restrict_when_unauthorized_button.dart
+++ src/frontend/lib/general/widgets/restrict_when_unauthorized_button.dart
@@ -35,11 +35,61 @@ class RestrictWhenUnauthorizedButton extends StatelessWidgetX {
                           color: Colors.white,
                         ),
                       ),
-                      content: Text(
-                        'Please sign-in to get access',
-                        style: GoogleFonts.raleway(
-                          color: Colors.white,
-                        ),
+                      content: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            'Please sign-in to get access:',
+                            style: GoogleFonts.raleway(
+                              color: Colors.white,
+                            ),
+                          ),
+                          SizedBox(height: 12),
+                          Row(
+                            mainAxisSize: MainAxisSize.min,
+                            children: [
+                              Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    '1) Connect',
+                                    style: GoogleFonts.raleway(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    '2) Sign-in',
+                                    style: GoogleFonts.raleway(
+                                      color: Colors.white,
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.bold,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                              SizedBox(width: 12),
+                              Column(
+                                children: [
+                                  Icon(
+                                    Icons.wifi_tethering,
+                                    color: Colors.white,
+                                    size: 17,
+                                  ),
+                                  SizedBox(height: 8),
+                                  Icon(
+                                    Icons.door_sliding,
+                                    color: Colors.white,
+                                    size: 17,
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
+                        ],
                       ),
                     ),
                   ),
